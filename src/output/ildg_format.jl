3e7408e935d50f5cd1beb1ea5bcf91d3153a9316
ho <hohsiao.phys@gmail.com>
2025-12-04 17:50:05 +0900

    remove useless code

--- src/output/ildg_format.jl
+++ src/output/ildg_format.jl
@@ -244,29 +244,16 @@ function __init__()
             myrank = U[1].myrank
             comm = MPI.COMM_WORLD
             PN = U[1].PN            
-            nprocs = U[1].nprocs
 
             Nfields = NC * NC * 4
-            total_sites = NX * NY * NZ * NT
-
-            # Preallocate recv buffer for each rank
             local_volume = prod(PN)
-            local_data = Vector{ComplexF64}(undef, Nfields * local_volume)
 
             bi = Binarydata_ILDG(filename, precision)
-            #bi.count += 
-
-            #offset reading for each rank
-            #dump = 0
-            #for _ in 1:(myrank* Nfields * local_volume)
-            #    dump = read!(bi)
-            #end
-            bytes_per_complex = 2 * sizeof(bi.floattype)
-            bytes_per_site = Nfields * bytes_per_complex
+            
+            bytes_per_site = 2 * sizeof(bi.floattype) * Nfields
             offset = myrank * local_volume * bytes_per_site
             seek(bi.fp, offset)
 
-            println("Rank $myrank: Starting to read binary data from file $(bi.count)")
             # All ranks: unpack local_data into U
             for it = 1:PN[4], iz = 1:PN[3], iy = 1:PN[2], ix = 1:PN[1]
                 for μ = 1:4
